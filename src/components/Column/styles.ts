ff4bf5cd4846945d86489d47ce884d2f434429dd
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-09-26 18:24:16 +0300

    Updated size of columns

--- src/components/Column/styles.ts
+++ src/components/Column/styles.ts
@@ -1,12 +1,17 @@
 const styles = {
     container: {
         width: 300,
+        height: 400
     },
     cardSegmentDropped: {
+        width: '100%',
+        height: '100%',
         background: "white",
         padding: 10,
     },
     cardSegment: {
+        width: '100%',
+        height: '100%',
         background: "grey",
         padding: 10,
     }
