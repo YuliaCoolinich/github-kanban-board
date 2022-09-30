c64fb25c23f799276a88b601f42546c78cc29ece
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-09-30 21:35:45 +0300

    Added validation error

---
+++ src/components/Navigation/errors/errorTypes.ts
@@ -0,0 +1,6 @@
+import { DOMAIN } from '../../../data/urls';
+
+export const ERROR_VALIDATION_TYPES = {
+    INCORRECT_DOMAIN: `Incorrect domain, use ${DOMAIN}`,
+    EXTRA_SLASH: `Incorrect URL, check the number of slashes / in your URL`,
+}
