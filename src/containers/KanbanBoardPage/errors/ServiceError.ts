1cb6cc999a3fdc871ed7fdb74646e60d0d2e792f
Yulia Kulinich <yuliakulinichsoft@gmail.com>
2022-11-10 23:07:16 +0200

    Added to ServiceError class message by default

--- src/containers/KanbanBoardPage/errors/ServiceError.ts
+++ src/containers/KanbanBoardPage/errors/ServiceError.ts
@@ -1,5 +1,7 @@
+import { ERROR_API_SERVICE_TYPES } from './errorTypes';
+
 class ServiceError extends Error {
-    constructor(message: string) {
+    constructor(message: string = ERROR_API_SERVICE_TYPES.INCORRECT_REQUEST) {
         super(message);
         this.name = "Service Error";
         this.cause = "Service";
